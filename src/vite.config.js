0e55896ae4e6e7e2c4701b0bcbb59291272c3b50
kazukikanomata <kazu.surface.pro2020@outlook.jp>
2025-04-26 22:33:00 +0900

    feat: タスク作成をReactに書き換え

--- src/vite.config.js
+++ src/vite.config.js
@@ -4,25 +4,26 @@ import react from '@vitejs/plugin-react';
 import fs from 'fs';
 
 export default defineConfig({
-    server: {
-        https: {
-            key: fs.readFileSync('./localhost-key.pem'),
-            cert: fs.readFileSync('./localhost.pem'),
-        },
-        host: 'localhost',
-        port: 5173,
+  server: {
+    https: {
+      key: fs.readFileSync('./localhost-key.pem'),
+      cert: fs.readFileSync('./localhost.pem'),
     },
-    plugins: [
-        // Laravelと連携するためのプラグイン
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/pages/TopPage.jsx',
-                'resources/js/pages/TasksIndexPage.tsx',
-                'resources/js/pages/TaskShowPage.jsx'            
-            ],
-            refresh: true,
-        }),
-        react(),
-    ],
+    host: 'localhost',
+    port: 5173,
+  },
+  plugins: [
+    // Laravelと連携するためのプラグイン
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/pages/TopPage.jsx',
+        'resources/js/pages/TasksIndexPage.tsx',
+        'resources/js/pages/TaskShowPage.jsx',
+        'resources/js/pages/TaskCreatePage.jsx',
+      ],
+      refresh: true,
+    }),
+    react(),
+  ],
 });
